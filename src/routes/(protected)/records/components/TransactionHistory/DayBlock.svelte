3e95acdc6a346620845df33d0db1cd4fcd5aaa07
Kostya <rayjackson@live.ru>
2025-09-03 21:48:23 +0300

    Transaction history block

---
+++ src/routes/(protected)/records/components/TransactionHistory/DayBlock.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+  import { categories } from '$stores/categories'
+  import { transactionTypes } from '$stores/transactionTypes'
+  import type { Option, Transaction } from '$types/forms'
+  import { parseDateFromISOString } from '$utils/dates'
+  import clsx from 'clsx'
+  import { format } from 'date-fns'
+  import { ru } from 'date-fns/locale'
+
+  type Props = {
+    dateISO: string
+    transactions: Transaction[]
+  }
+
+  const { dateISO, transactions }: Props = $props()
+  const date = parseDateFromISOString(dateISO)
+  const formatter = Intl.NumberFormat('ru', {
+    maximumFractionDigits: 0,
+    minimumFractionDigits: 0,
+  })
+
+  function getTransactionType(transaction: Transaction): Option | null {
+    return transactionTypes.find((type) => type.value === transaction.type) ?? null
+  }
+
+  function getCategory(transaction: Transaction): string | null {
+    return $categories.find((cat) => cat.value === transaction.category)?.name ?? null
+  }
+</script>
+
+<div class="flex flex-col select-none">
+  <div class="px-4 py-1 text-xs leading-[16px] opacity-70">
+    {date ? format(date, 'd MMMM', { locale: ru }) : '<дата не указана>'}
+  </div>
+
+  {#each transactions as item (item.id)}
+    {@const ribbonColor = getTransactionType(item)?.ribbon ?? 'bg-gray-300'}
+    {@const sign = item.type === 'Income' ? '+' : '-'}
+
+    <div
+      class="flex h-10 items-center gap-2 px-4 py-1 transition-colors duration-50 hover:bg-gray-100"
+    >
+      <div class={clsx('h-full w-[2px] rounded bg-red-300', ribbonColor)}></div>
+
+      <div class={clsx('flex h-8 flex-1 flex-col')}>
+        <p class="text-sm leading-[1.2]">{item.description || '<нет описания>'}</p>
+
+        <span class="text-xs leading-[1.2] opacity-70">
+          {getCategory(item) ?? '<без категории>'}
+        </span>
+      </div>
+
+      <p class={clsx('text-sm', item.type === 'Income' && 'text-green-600')}>
+        {sign}{formatter.format(item.amount ?? 0)} &#x20BD;
+      </p>
+    </div>
+  {/each}
+</div>
--- src/routes/(protected)/records/components/TransactionHistory/View.svelte
+++ src/routes/(protected)/records/components/TransactionHistory/View.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import type { Transaction } from '$types/forms'
+  import DayBlock from './DayBlock.svelte'
   import Header from './Header.svelte'
 
   type Props = {
@@ -7,13 +8,32 @@
   }
 
   let searchValue = $state('')
-  let { transactions }: Props = $props()
+  const { transactions }: Props = $props()
+  const flatList = Array.from(transactions.values()).flat()
+  const transactionSum = Array.from(transactions.values())
+    .flat()
+    .reduce((total, item) => (total += item?.amount ?? 0), 0)
 
-  $inspect(transactions)
+  const formatter = Intl.NumberFormat('ru', {
+    maximumFractionDigits: 0,
+    minimumFractionDigits: 0,
+  })
 </script>
 
 <div class="Card flex-1">
   <Header bind:searchValue />
 
-  <div class="flex flex-1 flex-col gap-2 py-2"></div>
+  <div class="flex flex-1 flex-col gap-2 py-2">
+    {#each Array.from(transactions) as [dateISO, array] (dateISO)}
+      <DayBlock {dateISO} transactions={array} />
+    {/each}
+  </div>
+
+  <div class="Card-footer min-h-8! justify-between">
+    <span class="text-xs opacity-70">Всего записей: {flatList.length}</span>
+
+    <span class="text-xs opacity-70">
+      Сумма транзакций: {formatter.format(transactionSum)} &#x20BD;
+    </span>
+  </div>
 </div>
